9a034e5a0e9227d2a4b8b7a03f3cdf173da20980
yegfomo <yegking@163.com>
2025-11-20 18:06:34 +0800

    时间锁

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -22,18 +22,20 @@ contract Timelock{
 
     address public admin; // 管理员地址
 
-    uint public constant  GRACE_PERIOD = 7 days; 
+    uint public constant  GRACE_PERIOD = 7 days; //交易过期时间。
     uint public delay; // 交易锁定时间 （秒）
     mapping (bytes32 => bool) public queuedTransactions; // txHash到bool，记录所有在时间锁队列中的交易
 
     //Timelock有两个修饰器
     modifier onlyOwner() {
+        //这里相当于调用合约发起人和当前合约地址比较。
         require(msg.sender == admin, "Timelock: Caller not admin");
         _;
     }
 
     // onlyTimelock modifier
     modifier onlyTimelock() {
+        //这里相当于调用合约发起人和当前合约地址比较。
         require(msg.sender == address(this), "Timelock: Caller not Timelock");
         _;
     }
@@ -49,7 +51,7 @@ contract Timelock{
      /**
      * @dev 改变管理员地址，调用者必须是Timelock合约。
      */
-     functio changeAdmin(address newAdmin) public onlyTimelock{
+     function changeAdmin(address newAdmin) public onlyTimelock{
         admin = newAdmin;
         emit NewAdmin(newAdmin);
      }
@@ -68,7 +70,12 @@ contract Timelock{
         // 检查：交易执行时间满足锁定时间
         require(executeTime >= getBlockTimestamp() + delay, "Timelock::queueTransaction: Estimated execution block must satisfy delay.");
         // 计算交易的唯一识别符：一堆东西的hash
+        bytes32 txHash = getTxHash(target, value, signature, data, executeTime);
+        // 将交易添加到队列
+        queuedTransactions[txHash] = true;
 
+        emit QueueTransaction(txHash, target, value, signature, data, executeTime);
+        return txHash; 
 
     }
 
@@ -77,7 +84,69 @@ contract Timelock{
      *
      * 要求：交易在时间锁队列中
      */
+     function cancelTransaction(address target, uint256 value, string memory signature, bytes memory data, uint256 executeTime) public onlyOwner{
+        /// 计算交易的唯一识别符：一堆东西的hash
+        bytes32 txHash = getTxHash(target, value, signature, data, executeTime);
+         // 检查：交易在时间锁队列中
+         require(queuedTransactions[txHash],"Timelock::cancelTransaction: Transaction hasn't been queued.");
+         // 将交易移出队列
+         queuedTransactions[txHash] = false;
+        emit CancelTransaction(txHash, target, value, signature, data, executeTime);
+     }
 
+    /**
+     * @dev 执行特定交易。
+     *
+     * 要求：
+     * 1. 交易在时间锁队列中
+     * 2. 达到交易的执行时间
+     * 3. 交易没过期
+     */
+     function executeTransaction(address target, uint256 value, string memory signature, bytes memory data, uint256 executeTime) public payable onlyOwner returns (bytes memory) {
+         bytes32 txHash = getTxHash(target, value, signature, data, executeTime);
+         // 检查：交易是否在时间锁队列中
+         require(queuedTransactions[txHash], "Timelock::executeTransaction: Transaction hasn't been queued.");
+         // 检查：达到交易的执行时间
+         require(getBlockTimestamp() >= executeTime, "Timelock::executeTransaction: Transaction hasn't surpassed time lock.");
+         // 检查：交易没过期
+         require(getBlockTimestamp() <= executeTime + GRACE_PERIOD, "Timelock::executeTransaction: Transaction is stale.");
+         // 将交易移出队列
+         queuedTransactions[txHash] = false;
 
+         // 获取call data
+         bytes memory callData;
+         if(bytes(signature).length == 0){
+            callData = data;
+         } else {
+            callData = abi.encodePacked(bytes4(keccak256(bytes(signature))),data);
+         }
+         //利用call执行交易
+         (bool success, bytes memory returnData) = target.call{value: value}(callData);
+         require(success, "Timelock::executeTransaction: Transaction execution reverted.");
+
+         emit ExecuteTransaction(txHash, target, value, signature, data, executeTime);
+         return returnData;
+
+     }
+
+      /**
+     * @dev 获取当前区块链时间戳
+     */
+     function getBlockTimestamp() public view returns (uint) {
+        return block.timestamp;
+    }
+
+     /**
+     * @dev 将一堆东西拼成交易的标识符
+     */
+     function getTxHash(
+        address target,
+        uint value,
+        string memory signature,
+        bytes memory data,
+        uint executeTime
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encode(target, value, signature, data, executeTime));
+    }
 
 }
